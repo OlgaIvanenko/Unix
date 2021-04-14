d5af58b66ff7b0b4789cd349058fab62c014f736
OlgaIvanenko <63466451+OlgaIvanenko@users.noreply.github.com>
2021-04-14 10:50:53 +0300

    Delete undeploy.sh

--- undeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -u tomcat:s3cret "http://localhost:8080/manager/text/stop?path=/sample"
-
-curl -u tomcat:s3cret "http://localhost:8080/manager/text/undeploy?path=/sample&sample"
-
-
